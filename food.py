77ba59f21d0d84495d8383e4da7aea6ec6f65043
leenuu <61344683+leenuu@users.noreply.github.com>
2020-07-22 13:34:21 +0900

    Update food.py

--- food.py
+++ food.py
@@ -13,6 +13,7 @@ data = list()
 text_data = '\n'
 text = ''
 text_ = ''
+te = ''
 er = 0
 
 url = f'http://jeil.jje.hs.kr/jeil-h/food/2020/{dy.datetime.today().month}/{dy.datetime.today().day}/lunch'
@@ -33,11 +34,9 @@ except AttributeError:
   imgg = ''
   er = 1
 
-for i in soup.find_all('dd'):
-  # print(i.get_text())
-  data.append(i.get_text())
+te = str(soup.find_all('dd')[1]).replace('<br/>','\n').replace('<dd>','').replace('</dd>','')
 
-for i in data[1]:
+for i in te:
   try:
     int(i)
   except ValueError:
@@ -49,26 +48,29 @@ text = text.split('.')
 for i in range(len(text)):
   if text[i] != '' :
     if ')' in text[i]:
-      text_data += text[i][:text[i].index(")")+1] + '\n'
+      text_data += text[i][:text[i].index(")")+1] 
     else:
-      text_data += text[i] + '\n'
+      text_data += text[i] 
   else:
     text_data += '' 
 
 if '%' in text_data:
   print('1')
-  text_data = text_data.replace('%','')
+  text_data = text_data.replace('%','') + '\n'
 
 
 win=tkinter.Tk()
 win.title("급식")
-win.geometry("640x740+100+100")
+win.geometry("690x760+100+100")
 win.resizable(False, False)
 
 # req_btn = tkinter.Button(win, text="급식", overrelief="solid", command=req, repeatdelay=1000, repeatinterval=100)
 # req_btn.pack()
 
-font = tkinter.font.Font(family="맑은 고딕" ,size=16)
+font = tkinter.font.Font(family="맑은 고딕" ,size=14)
+
+date_label = tkinter.Label(win, text=f'{dy.datetime.today().month}월 {dy.datetime.today().day}일 점심 식단', font=font)
+date_label.pack()
 
 res_label = tkinter.Label(win ,text=text_data ,font=font)
 res_label.pack()
@@ -82,7 +84,7 @@ if er == 0:
 
 img_label = tkinter.Label(win, image = imgg)
 if er == 1:
-  img_label.config(text='음식사진 업로드 안됨 \n 3교시이후 업로드 예정', font=font)
+  img_label.config(text='\n\n음식사진 업로드 안됨 \n 3교시이후 업로드 예정', font=font)
 img_label.pack()
 
 win.mainloop()
