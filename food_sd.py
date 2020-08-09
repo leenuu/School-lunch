ff110742772f6d53a43866595442303a095ee740
leenuu <61344683+leenuu@users.noreply.github.com>
2020-08-09 21:03:29 +0900

    gui do not use .ui file

--- food_sd.py
+++ food_sd.py
@@ -9,11 +9,8 @@ import urllib.request
 import cv2
 import os 
 import sys
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
+from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5 import uic
-from PyQt5 import QtCore
-
 #################################################### food ######################################################################################
 
 data = list()
@@ -22,8 +19,8 @@ text = ''
 text_ = ''
 te = ''
 er = 0
-# url = f'http://jeil.jje.hs.kr/jeil-h/food/2020/{dy.datetime.today().month}/{dy.datetime.today().day}/lunch'
-url = "http://jeil.jje.hs.kr/jeil-h/food/2020/7/31/lunch"
+url = f'http://jeil.jje.hs.kr/jeil-h/food/2020/{dy.datetime.today().month}/{dy.datetime.today().day}/lunch'
+# url = "http://jeil.jje.hs.kr/jeil-h/food/2020/7/31/lunch"
 
 ua = UserAgent()
 header = {'User-Agent':str(ua.chrome)}
@@ -93,8 +90,8 @@ try:
     soup = BeautifulSoup(html,'html.parser')
 
     for i in soup.find('tbody').find_all('a'):
-        # if f'{dy.datetime.today().month}/{dy.datetime.today().day}' in str(i): 
-        if f'{7}/{31}' in str(i): 
+        if f'{dy.datetime.today().month}/{dy.datetime.today().day}' in str(i): 
+        # if f'{7}/{31}' in str(i): 
             sd_data = i
 
     data_url = str(sd_data.get('onclick'))[str(sd_data.get('onclick')).index('(')+1:str(sd_data.get('onclick')).index(')')].split(',')[1].replace("'",'')
@@ -162,16 +159,49 @@ except AttributeError:
 #################################################### gui ######################################################################################
 ft = "굴림"
 sz = 14
-form_class = uic.loadUiType("test.ui")[0]
 
-class WindowClass(QMainWindow, form_class) :
-    def __init__(self) :
-        super().__init__()
-        self.setupUi(self)
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(878, 360)
+        self.classnum = QtWidgets.QComboBox(Form)
+        self.classnum.setGeometry(QtCore.QRect(10, 40, 71, 20))
+        self.classnum.setObjectName("classnum")
+        self.classnum.addItem("")
+        self.classnum.addItem("")
+        self.classnum.addItem("")
+        self.classnum.addItem("")
+        self.classnum.addItem("")
+        self.classnum.addItem("")
+        self.classnum.addItem("")
+        self.classnum.addItem("")
+        self.classnum.addItem("")
+        self.classnum.addItem("")
+        self.classnum.addItem("")
+        self.classnum.addItem("")
+        self.classnum.addItem("")
+        self.time_label = QtWidgets.QLabel(Form)
+        self.time_label.setGeometry(QtCore.QRect(10, 80, 201, 211))
+        self.time_label.setText("")
+        self.time_label.setAlignment(QtCore.Qt.AlignHCenter|QtCore.Qt.AlignTop)
+        self.time_label.setObjectName("time_label")
+        self.pushButton = QtWidgets.QPushButton(Form)
+        self.pushButton.setGeometry(QtCore.QRect(130, 40, 81, 23))
+        self.pushButton.setObjectName("pushButton")
+        self.food_label = QtWidgets.QLabel(Form)
+        self.food_label.setGeometry(QtCore.QRect(250, 80, 231, 211))
+        self.food_label.setAlignment(QtCore.Qt.AlignHCenter|QtCore.Qt.AlignTop)
+        self.food_label.setObjectName("food_label")
+        self.img_label = QtWidgets.QLabel(Form)
+        self.img_label.setGeometry(QtCore.QRect(510, 0, 360, 360))
+        self.img_label.setAlignment(QtCore.Qt.AlignHCenter|QtCore.Qt.AlignTop)
+        self.img_label.setObjectName("img_label")
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
         self.pushButton.clicked.connect(self.get_time)
-        self.food_label.setFont(QFont(ft, sz))
-        self.time_label.setFont(QFont(ft, sz))
-        self.img_label.setFont(QFont(ft, sz))
+        self.food_label.setFont(QtGui.QFont(ft, sz))
+        self.time_label.setFont(QtGui.QFont(ft, sz))
+        self.img_label.setFont(QtGui.QFont(ft, sz))
         self.food_label.setText(text_data)
         if er == 0:
           self.qPixmapFileVar = QPixmap()
@@ -181,8 +211,6 @@ class WindowClass(QMainWindow, form_class) :
         else:
           self.img_label.setText("\n\n\n음식사진 업로드 안됨 \n 3교시이후 업로드 예정")
 
-    def button1Function(self):
-      print("btn_1 Clicked")
 
     def get_time(self):
       try:
@@ -208,13 +236,41 @@ class WindowClass(QMainWindow, form_class) :
       except NameError:
         self.time_label.setText("시간표 업로드 안됨")
 
-if __name__ == "__main__" :
-  app = QApplication(sys.argv) 
-  myWindow = WindowClass() 
-  myWindow.show()
-  app.exec_()
 
-  if er == 0:
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.classnum.setItemText(0, _translate("Form", "1반"))
+        self.classnum.setItemText(1, _translate("Form", "2반"))
+        self.classnum.setItemText(2, _translate("Form", "3반"))
+        self.classnum.setItemText(3, _translate("Form", "4반"))
+        self.classnum.setItemText(4, _translate("Form", "5반"))
+        self.classnum.setItemText(5, _translate("Form", "6반"))
+        self.classnum.setItemText(6, _translate("Form", "7반"))
+        self.classnum.setItemText(7, _translate("Form", "8반"))
+        self.classnum.setItemText(8, _translate("Form", "9반"))
+        self.classnum.setItemText(9, _translate("Form", "10반"))
+        self.classnum.setItemText(10, _translate("Form", "11반"))
+        self.classnum.setItemText(11, _translate("Form", "12반"))
+        self.classnum.setItemText(12, _translate("Form", "13반"))
+        self.pushButton.setText(_translate("Form", "조회"))
+        self.food_label.setText(_translate("Form", "TextLabel"))
+        self.img_label.setText(_translate("Form", "img"))
+
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    Form = QtWidgets.QWidget()
+    ui = Ui_Form()
+    ui.setupUi(Form)
+    Form.show()
+    sys.exit(app.exec_())
+
     os.remove('img.jpg')
-  os.remove('sd.xlsx')
-  os.remove(f"시간표 {sh_name}.xlsx")
+    os.remove('sd.xlsx')
+    os.remove(f"시간표 {sh_name}.xlsx")
+
+
+
