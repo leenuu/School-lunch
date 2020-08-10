1fe9d168f4f9f357ce1b78d6d22d73f5dc63b8ae
leenuu <61344683+leenuu@users.noreply.github.com>
2020-08-10 15:20:55 +0900

    test

--- update.py
+++ update.py
@@ -1,13 +1,12 @@
 from fake_useragent import UserAgent
 from bs4 import BeautifulSoup
+from tqdm import tqdm
 import requests
 import os 
 import sys
 import urllib.request
 import time
 
-start = time.time()
-path = ''
 url = 'https://github.com/leenuu/School-lunch-and-school-schedule/blob/master/update.txt'
 ua = UserAgent()
 header = {'User-Agent':str(ua.chrome)}
@@ -16,13 +15,15 @@ html = req_html.text
 soup = BeautifulSoup(html,'html.parser')
 
 if int(soup.find('div', class_='Box-body p-0 blob-wrapper data type-text').find('td', id='LC1').get_text()) == 1:
-    print('update')
-    url = 'https://github.com/leenuu/food_sd/archive/master.zip'
-    urllib.request.urlretrieve(url,'update.zip')
-end = time.time()
 
-print(end - start)
-    
+    chunk_size = 1024
+    url = 'https://github.com/leenuu/food_sd/archive/master.zip'
+    with open('update.zip', "wb") as file:
+        response = requests.get(url)              
+        file.write(response.content)
+
+
+
 
 
 
