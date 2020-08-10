af3f9f5e7d483e01a264d0debfd9b93fc7ff55bf
leenuu <61344683+leenuu@users.noreply.github.com>
2020-08-10 13:52:27 +0900

    update

--- update.py
+++ update.py
@@ -3,32 +3,10 @@ from bs4 import BeautifulSoup
 import requests
 import os 
 import sys
-import shutil
-import subprocess
-import ctypes
-import zipfile
 import urllib.request
+import time
 
-
-def is_admin():
-    try:
-        return ctypes.windll.Shell32.IsUserAnAdmin()
-    except:
-        return False
-
-def unzip(source_file, dest_path):
-    with zipfile.ZipFile(source_file, 'r') as zf:
-        zf.extractall(path=dest_path)
-        zf.close()
-
-def upd():
-    print('update')
-    url = 'https://github.com/leenuu/food_sd/archive/master.zip'
-    urllib.request.urlretrieve(url,'update.zip')
-    unzip(os.getcwd() + '/update.zip', os.getcwd())
-    os.remove(os.getcwd() + '/update.zip')
-
-
+start = time.time()
 path = ''
 url = 'https://github.com/leenuu/School-lunch-and-school-schedule/blob/master/update.txt'
 ua = UserAgent()
@@ -37,13 +15,13 @@ req_html = requests.get(url, headers=header)
 html = req_html.text
 soup = BeautifulSoup(html,'html.parser')
 
-
 if int(soup.find('div', class_='Box-body p-0 blob-wrapper data type-text').find('td', id='LC1').get_text()) == 1:
-    if is_admin():
-        upd()
-    else:
-        ctypes.windll.Shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
-        upd()
+    print('update')
+    url = 'https://github.com/leenuu/food_sd/archive/master.zip'
+    urllib.request.urlretrieve(url,'update.zip')
+end = time.time()
+
+print(end - start)
     
 
 
